9fefebce671ddb5fad18d2bb001050ab98b2de21
Gabor Szabo <szabgab@gmail.com>
2012-05-17 23:42:27 +0300

    space

--- bin/convert.pl
+++ bin/convert.pl
@@ -11,23 +11,24 @@ die 'has pm.db' if -e 'pm.db';
 my $dsn = "dbi:SQLite:dbname=pm.db";
 DBIx::RunSQL->create(
 	verbose => 0,
-    dsn     => $dsn,
-    sql     => 'sql/schema.sql',
+	dsn     => $dsn,
+	sql     => 'sql/schema.sql',
 );
 
 my $dbh = DBI->connect($dsn, "", "", {
-     RaiseError => 1,
-	 PrintError => 0,
-	 AutoCommit => 1,
+	RaiseError => 1,
+	PrintError => 0,
+	AutoCommit => 1,
 });
 
 my $data = LoadFile('t/data.yml');
 foreach my $email (sort { $data->{$a}{register} cmp $data->{$b}{register} }  keys %$data) {
 	#print "$email  $data->{$email}{register} $data->{$email}{code} $data->{$email}{verified}\n";
-    $dbh->do('INSERT INTO user (email, register_time, verify_code, verify_time)
-	    VALUES (?, ?, ?, ?)',
-        undef,
-	    $email, $data->{$email}{register}, $data->{$email}{code}, $data->{$email}{verified});
-    my $id = $dbh->last_insert_id('', '', '', '');
-    #print "$id\n";
+	$dbh->do('INSERT INTO user (email, register_time, verify_code, verify_time)
+		VALUES (?, ?, ?, ?)',
+		undef,
+		$email, $data->{$email}{register}, $data->{$email}{code}, $data->{$email}{verified});
+	my $id = $dbh->last_insert_id('', '', '', '');
+	#print "$id\n";
 }
+
