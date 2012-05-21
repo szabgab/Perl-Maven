5308ca1302c0eeee0080023f23048f1ce2033324
Gabor Szabo <szabgab@gmail.com>
2012-05-21 20:51:22 +0300

    remove limit that was used for testing only

--- bin/convert2.pl
+++ bin/convert2.pl
@@ -24,7 +24,7 @@ my $dbh = DBI->connect($dsn, "", "", {
 $dbh->do('INSERT INTO product (dir, name) VALUES (?, ?)',
 	undef, 'perl_maven_cookbook', 'Perl Maven Cookbook');
 
-my $users = $dbh->selectall_arrayref('SELECT id FROM user WHERE id < 5');
+my $users = $dbh->selectall_arrayref('SELECT id FROM user');
 #die Dumper $users;
 
 foreach my $ar (@$users) {
