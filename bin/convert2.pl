ec4031ce95fece834da45dd2de5f968ba9ffaf44
Gabor Szabo <szabgab@gmail.com>
2012-05-21 20:37:08 +0300

    rename and fix convert2

--- bin/convert2.pl
+++ bin/convert2.pl
@@ -2,8 +2,9 @@ use strict;
 use warnings;
 
 use DBI;
+use Data::Dumper qw(Dumper);
 
-die 'has pm.db' if -e 'pm.db';
+die 'has no pm.db' if ! -e 'pm.db';
 
 my $dsn = "dbi:SQLite:dbname=pm.db";
 my $dbh = DBI->connect($dsn, "", "", {
@@ -18,7 +19,7 @@ $dbh->do('INSERT INTO product (dir, name) VALUES (?)',
 my $users = $dbh->selectall_arrayref('SELECT id FROM user WHERE id < 5');
 print Dumper $users;
 
-$dbh->do('INSERT INTO  subscription (uid, pid) VALUES (?, ?)',
-	undef, $uid, 1);
+#$dbh->do('INSERT INTO  subscription (uid, pid) VALUES (?, ?)',
+#	undef, $uid, 1);
 
 
