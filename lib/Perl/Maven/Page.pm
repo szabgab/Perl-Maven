9a82d89906c3394370a4f2f3506172893883cd26
Gabor Szabo <szabgab@gmail.com>
2012-08-04 17:43:34 +0300

    highlight hl tags

--- lib/Perl/Maven/Page.pm
+++ lib/Perl/Maven/Page.pm
@@ -12,8 +12,8 @@ sub read {
 	my $in_code;
 	if (open my $fh, '<', $self->file) {
 		while (my $line = <$fh>) {
-			$line =~ s{<hl>}{<b>}g;
-			$line =~ s{</hl>}{</b>}g;
+			$line =~ s{<hl>}{<span class="label">}g;
+			$line =~ s{</hl>}{</span>}g;
 			if ($line =~ /^=abstract start/ .. $line =~ /^=abstract end/) {
 				next if $line =~ /^=abstract/;
 				$data{abstract} .= $line;
