496a813b46fb45ec85d629a28f64f62067ef2a68
Gabor Szabo <szabgab@gmail.com>
2013-01-07 11:51:07 +0200

    muc stricter header checking

--- lib/Perl/Maven/Page.pm
+++ lib/Perl/Maven/Page.pm
@@ -10,7 +10,43 @@ sub read {
 	my %data = (content => '', abstract => '');
 	my $cont = '';
 	my $in_code;
-	if (open my $fh, '<', $self->file) {
+    my @header = qw(title timestamp description? indexes? tags? status standalone? clean? author index archive feed comments social);
+
+
+    my $file = $self->file;
+
+	if (open my $fh, '<', $file) {
+        for (my $i = 0; $i <= $#header; $i++) {
+            my $field = $header[$i];
+
+            my $line = <$fh>;
+            chomp $line;
+            if ($line =~ /^\s*$/) {
+                die "Header ended and '$field' was not supplied for file $file\n";
+            }
+
+            if (my ($f, $v) = $line =~ /^=(\w+)\s+(.*?)\s*$/) {
+				$data{$f} = $v;
+                while ($f ne $field and "$f?" ne $field) {
+                    if (substr($field, -1) eq '?') {
+                        $i++;
+                        if ($i > $#header) {
+                            die "We ran out of fields while processing line '$line' in file $file\n";
+                        }
+                        $field = $header[$i];
+                        next;
+                    }
+                    die "Invalid entry in header expected '$field', received '$f' in line '$line' file $file\n";
+                }
+			} else {
+                die "Invalid entry in header for '$field' in line '$line' file $file\n";
+            }
+        }
+        my $line = <$fh>;
+        if ($line =~ /\S/) {
+            die "Header not ended even after we ran out of required fields. line '$line' file $file\n";
+        }
+
 		while (my $line = <$fh>) {
 			#$line =~ s{<hl>}{<span class="">}g;
 			#$line =~ s{</hl>}{</span>}g;
@@ -22,10 +58,6 @@ sub read {
 				if ($line =~ /^\s*$/) {
 					$data{abstract} .= "<p>\n";
 				}
-			}
-			if ($line =~ /^=(\w+)\s+(.*?)\s*$/) {
-				$data{$1} = $2;
-				next;
 			}
 			if ($line =~ m{^<code(?: lang="([^"]+)")?>}) {
 				$in_code = $1;
