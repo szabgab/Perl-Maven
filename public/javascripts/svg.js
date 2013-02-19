2c01107d20b21630dbaf4fe2f4f515927ef1b3a5
Gabor Szabo <szabgab@gmail.com>
2013-02-19 18:59:28 +0200

    svg

--- public/javascripts/svg.js
+++ public/javascripts/svg.js
@@ -22,16 +22,16 @@ function fetch_perl_xml() {
 */
 
 function draw_xml() {
-    var header = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>'
-        + '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">';
+    var header = '<?xml version="1.0" encoding="UTF-8" standalone="yes"?>\n'
+        + '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.0//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">\n';
 
     var svg_start = '<svg xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"';
     //should be height and width
     $('#image').find("input").each(function(index, field) {
             svg_start += ' ' + field.name + '="' + field.value + '"';
     });
-    svg_start += '>';
-    var svg_end = '</svg>';
+    svg_start += '>\n';
+    var svg_end = '</svg>\n';
 
     var data = '<rect';
     $('#form').find("input").each(function(index, field) {
@@ -40,11 +40,12 @@ function draw_xml() {
             data += ' ' + field.name + '="' + field.value + '"';
         }
     })
-    data += ' />';
+    data += ' />\n';
 
     var xml = svg_start + data + svg_end;
     //console.log(xml);
     $("#svg-xml").html(xml);
+    $("#source-code").val(header + '\n' + xml);
     return false;
 }
 
