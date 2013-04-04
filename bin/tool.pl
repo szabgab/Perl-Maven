2e151fd2368668c80d15141ed386614482707603
Gabor Szabo <szabgab@gmail.com>
2013-04-05 01:21:31 +0300

    mirror index files

--- bin/tool.pl
+++ bin/tool.pl
@@ -7,24 +7,35 @@ use warnings;
 use Getopt::Long qw(GetOptions);
 use Perl::Maven::Tool;
 
-my %opt;
+my %opt = (
+	root => '/home/gabor/work/articles/cpan',
+	cpan => 'http://cpan.pair.com/',
+);
 GetOptions(\%opt,
 	'help',
 	'root=s',
+	'cpan=s',
+	'update',
 ) or usage();
 usage() if $opt{help};
 
-$opt{root} = '/home/gabor/work/articles/cpan';
-
 
 my $tool = Perl::Maven::Tool->new( %opt );
-$tool->list;
+if ($opt{update}) {
+	$tool->get_index_files;
+	exit;
+}
+
+#$tool->list;
 
 
 sub usage {
 	die <<"END_USAGE";
 Usage: $0
     --help         this help
-    --root PATH    to directory where the distributions are saved.
+    --root PATH    to directory where the distributions are saved. $opt{root}
+    --cpan URL     to your selected CPAN   $opt{cpan}
+
+    --update       update the index file from cpan
 END_USAGE
 }
--- lib/Perl/Maven/Tool.pm
+++ lib/Perl/Maven/Tool.pm
@@ -1,18 +1,43 @@
 package Perl::Maven::Tool;
 use Moo;
 
+use LWP::Simple qw(mirror);
+
 has root => (
 	is       => 'ro',
 	required => 1,
 );
 
-sub list {
+has cpan => (
+	is       => 'ro',
+);
+
+
+sub get_root {
 	my ($self) = @_;
 
 	die sprintf("root '%s' does not exist", $self->root) if not -e $self->root;
+	return $self->root;
+}
+
+sub get_index_files {
+	my ($self) = @_;
+	die 'URL of cpan not given' if not $self->cpan;
+	my $url = $self->cpan . '/modules/02packages.details.txt.gz';
+	my $root = $self->get_root;
+	my $dir = "$root/.cpan";
+	mkdir $dir if not -e $dir;
+	mirror($url, "$dir/02packages.details.txt.gz"); # TODO error checking?
+
+	return;
+}
+
+sub list {
+	my ($self) = @_;
+
+	my $root = $self->get_root;
 }
 
 
 1;
-
 
