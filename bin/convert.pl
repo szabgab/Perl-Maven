f21e7fb4cc91b02996843b22abfcee443533b572
Gabor Szabo <szabgab@gmail.com>
2013-07-12 08:04:22 +0300

    remove old schema files and conversion script

--- bin/convert.pl
+++ bin/convert.pl
@@ -5,7 +5,7 @@ use YAML qw(DumpFile LoadFile);
 use DBIx::RunSQL;
 use DBI;
 
-die 'has no t/data.yml' if not -e 't/data.yml';
+#die 'has no t/data.yml' if not -e 't/data.yml';
 die 'has pm.db' if -e 'pm.db';
 
 
@@ -22,14 +22,14 @@ my $dbh = DBI->connect($dsn, "", "", {
 	AutoCommit => 1,
 });
 
-my $data = LoadFile('t/data.yml');
-foreach my $email (sort { $data->{$a}{register} cmp $data->{$b}{register} }  keys %$data) {
-	#print "$email  $data->{$email}{register} $data->{$email}{code} $data->{$email}{verified}\n";
-	$dbh->do('INSERT INTO user (email, register_time, verify_code, verify_time)
-		VALUES (?, ?, ?, ?)',
-		undef,
-		$email, $data->{$email}{register}, $data->{$email}{code}, $data->{$email}{verified});
-	my $id = $dbh->last_insert_id('', '', '', '');
-	#print "$id\n";
-}
+#my $data = LoadFile('t/data.yml');
+#foreach my $email (sort { $data->{$a}{register} cmp $data->{$b}{register} }  keys %$data) {
+#	#print "$email  $data->{$email}{register} $data->{$email}{code} $data->{$email}{verified}\n";
+#	$dbh->do('INSERT INTO user (email, register_time, verify_code, verify_time)
+#		VALUES (?, ?, ?, ?)',
+#		undef,
+#		$email, $data->{$email}{register}, $data->{$email}{code}, $data->{$email}{verified});
+#	my $id = $dbh->last_insert_id('', '', '', '');
+#	#print "$id\n";
+#}
 
