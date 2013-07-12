739a482429471603f93ef8a02aac2a82218b5988
Gabor Szabo <szabgab@gmail.com>
2013-07-12 08:13:15 +0300

    dos2unix and load schema 4

--- bin/convert.pl
+++ bin/convert.pl
@@ -5,19 +5,18 @@ use YAML qw(DumpFile LoadFile);
 use DBIx::RunSQL;
 use DBI;
 
-die 'has pm.db' if -e 'pm.db';
-
+die 'has no pm.db yet' if not -e 'pm.db';
 
 my $dsn = "dbi:SQLite:dbname=pm.db";
 DBIx::RunSQL->create(
 	verbose => 0,
 	dsn     => $dsn,
-	sql     => 'sql/schema1.sql',
+	sql     => 'sql/schema4.sql',
 );
 
-my $dbh = DBI->connect($dsn, "", "", {
-	RaiseError => 1,
-	PrintError => 0,
-	AutoCommit => 1,
-});
+#my $dbh = DBI->connect($dsn, "", "", {
+#	RaiseError => 1,
+#	PrintError => 0,
+#	AutoCommit => 1,
+#});
 
