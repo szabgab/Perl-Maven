b92b251e70190744e2f6cf8eddbe13a67c848829
Gabor Szabo <gabor@szabgab.com>
2013-12-21 18:06:25 +0200

    add /explain using Code::Explain

--- lib/Perl/Maven.pm
+++ lib/Perl/Maven.pm
@@ -7,6 +7,7 @@ my $TIMEOUT = 60*60*24*365;
 my $MAX_INDEX   = 3;
 my $MAX_FEED    = 10;
 my $MAX_META_FEED = 20;
+my $CODE_EXPLAIN_LIMIT = 20;
 
 use Business::PayPal;
 use Cwd qw(cwd abs_path);
@@ -772,6 +773,54 @@ get qr{/media/(.+)} => sub {
 
 	return 'media error';
 };
+
+get '/explain' => sub {
+	require Code::Explain;
+	my %data = (
+		code_explain_version => $Code::Explain::VERSION,
+		limit                => $CODE_EXPLAIN_LIMIT,
+	);
+	return template 'explain', \%data;
+};
+post '/explain' => sub {
+	my $code = params->{'code'};
+	$code = '' if not defined $code;
+	$code =~ s/^\s+|\s+$//g;
+
+	my %data = (
+		code_explain_version => $Code::Explain::VERSION,
+		limit                => $CODE_EXPLAIN_LIMIT,
+		code                 => $code,
+	);
+	if ($code) {
+		$data{html_code} = _escape($code);
+		if (length $code > $CODE_EXPLAIN_LIMIT) {
+			$data{too_long} = length $code;
+		} else {
+			my $time = time;
+			my $log_file = path( config->{appdir}, 'logs', 'code_' .  POSIX::strftime("%Y%m", gmtime($time)));
+			if ( open my $fh, '>>', $log_file ) {
+				print $fh scalar(gmtime $time) . "\n";
+				print $fh "$code\n\n";
+				close $fh;
+			}
+			require Code::Explain;
+			my $ce = Code::Explain->new( code => $code );
+			$data{explanation} = $ce->explain();
+			$data{ppi_dump}    = [ map { _escape($_) } $ce->ppi_dump ];
+			$data{ppi_explain} = [ map { $_->{code} = _escape($_->{code}); $_ } $ce->ppi_explain ];
+		} 
+	}
+	return to_json \%data;
+};
+
+sub _escape {
+	my $txt = shift;
+	$txt =~ s/</&lt;/g;
+	$txt =~ s/>/&gt;/g;
+	return $txt;
+}
+
 
 
 
--- public/javascripts/perlmaven.js
+++ public/javascripts/perlmaven.js
@@ -57,7 +57,47 @@ function show_archive(tag, show_abstract) {
     window.location = url;
 }
 
+function code_explain() {
+    var code = $('#code').val();
+    var data = { "code" : code };
+    //$('#result').slideToggle('fast', function() {});
+    $('#result').show('fast', function() {});
+
+    $('#code_echo').empty();
+	$('#explanation').empty();
+	$('#ppi_explain').empty();
+	$('#ppi_dump').empty();
+
+    var code_html = code;
+    code_html = code_html.replace(/</g, "&lt;");
+    code_html = code_html.replace(/>/g, "&gt;");
+    $('#code_echo').append('<pre>' + code_html + '</pre>');
+	$.post("/explain", data, function(resp) {
+		//console.log('success');
+		//console.log(resp);
+		var data = jQuery.parseJSON(resp);
+
+    	$('#explanation').append(data["explanation"]);
+
+        var ppi_dump = data["ppi_dump"].join("\n");
+    	$('#ppi_dump').append('<pre>' + ppi_dump  + '</pre>');
+
+        var ppi_explain = '';
+		for(var i=0; i < data["ppi_explain"].length; i++) {
+			ppi_explain += '<b>' + data["ppi_explain"][i]["code"] + '</b>';
+			ppi_explain += data["ppi_explain"][i]["text"] + '<br>'; 
+		}
+    	$('#ppi_explain').append(ppi_explain);
+	}).fail(function() {
+		//console.log('fail');
+		alert('fail');
+	});
+    //alert($('#code').val());
+}
+
+
 $(document).ready(function() {
+    $('#explain').click(code_explain);
 
 	$(".archive-button").click(function (e) {
 		//console.log( $('#abstract').attr('checked') );
@@ -91,5 +131,7 @@ $(document).ready(function() {
 //		console.log(this);
 //		console.log($(this).attr('href'));
 //	});
+
+
 });
 
