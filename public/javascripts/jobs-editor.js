36ab7e57cffe4e9ee838d2bcd6f9059eca2eb5fd
Gabor Szabo <gabor@szabgab.com>
2014-05-09 07:40:48 +0300

    work on the jobs page

--- public/javascripts/jobs-editor.js
+++ public/javascripts/jobs-editor.js
@@ -5,13 +5,20 @@ function show_job() {
     if (title) {
         html = '<h2>' + title + '</h2>';
     }
-    
+    var description = $('#job_description').val();
+    if (description) {
+       html = markdown(description);
+    }
     console.log(html);
-    $('#job-post').html( html );
+    $('#job_post').html( html );
 }
 
 function show_error(txt) {
     console.log('Error: ' + txt);
+}
+
+function markdown(text) {
+    return text;
 }
 
 function save_job() {
@@ -25,7 +32,7 @@ function save_job() {
 
 function setup_jobs_editor() {
     console.log('setup starts 1');
-    $("#save").click(save_job);
+    $("#job_save").click(save_job);
     $("#job_title").change(show_job);
     $("#job_title").keyup(show_job);
     console.log('setup ends');
