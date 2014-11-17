9e90e40f2f858e3d5e0be40629daa69dd9b730c9
Gabor Szabo <gabor@szabgab.com>
2014-11-17 14:02:50 +0200

    remove the start function from the test that was forking a new server; we only use the Plack::Test module and the Mechanize::PSGI module from now

--- t/lib/Test.pm
+++ t/lib/Test.pm
@@ -4,7 +4,7 @@ use warnings;
 
 use base 'Exporter';
 
-our @EXPORT_OK = qw(start read_file psgi_start);
+our @EXPORT_OK = qw(read_file psgi_start);
 
 use Cwd qw(cwd);
 use File::Basename qw(basename);
@@ -15,7 +15,6 @@ use DBIx::RunSQL;
 
 my $dbfile;
 my $backup;
-my $process;
 
 sub setup {
 	my ($dir) = @_;
@@ -50,58 +49,7 @@ sub psgi_start {
 	setup();
 }
 
-sub start {
-	my $dir = tempdir( CLEANUP => 1 );
-
-	# print STDERR "# $dir\n";
-	my ($cnt) = split /_/, basename $0;
-
-	$ENV{PERL_MAVEN_TEST} = 1;
-	$ENV{PERL_MAVEN_PORT} = 20_000 + $cnt;
-	$ENV{PERL_MAVEN_MAIL} = File::Spec->catfile( $dir, 'mail.txt' );
-
-	setup();
-
-	my $root = cwd();
-	if ( $^O =~ /win32/i ) {
-		require Win32::Process;
-
-		#import Win32::Process;
-
-		Win32::Process::Create( $process, $^X,
-			"perl -Ilib -It\\lib $root\\bin\\app.pl",
-			0, Win32::Process::NORMAL_PRIORITY_CLASS(), '.' )
-			|| die ErrorReport();
-	}
-	else {
-		$process = fork();
-
-		die "Could not fork() while running on $^O" if not defined $process;
-
-		if ($process) {    # parent
-			sleep 1;
-			return $process;
-		}
-
-		my $cmd = "$^X -Ilib -It/lib $root/bin/app.pl";
-		exec $cmd;
-	}
-
-	return 1;
-}
-
-sub stop {
-	return if not $process;
-	if ( $^O =~ /win32/i ) {
-		$process->Kill(0);
-	}
-	else {
-		kill 9, $process;
-	}
-}
-
 END {
-	stop();
 	if ($backup) {
 		move $backup, $dbfile;
 	}
