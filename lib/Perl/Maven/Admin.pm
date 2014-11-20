4ccb008024f74d746017acd22764ecdc49a84a44
Gabor Szabo <gabor@szabgab.com>
2014-11-20 09:55:13 +0200

    admin page now requires logged in user

--- lib/Perl/Maven/Admin.pm
+++ lib/Perl/Maven/Admin.pm
@@ -1,15 +1,18 @@
 package Perl::Maven::Admin;
 use Dancer ':syntax';
-use Perl::Maven::DB;
+
+#use Perl::Maven::DB;
+use Perl::Maven::WebTools qw(logged_in);
 
 our $VERSION = '0.11';
 
 get '/admin' => sub {
+	if ( not logged_in() ) {
+		session url => request->path;
+		return redirect '/login';
+	}
 	return 'Admin';
 };
 
 true;
-
-# vim:noexpandtab
-# vim:ts=4
 
