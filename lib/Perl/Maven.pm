5d470b616b7c067a3308ff8e58abffd95976e75f
Gabor Szabo <gabor@szabgab.com>
2014-12-01 21:36:01 +0200

    another 2 bots

--- lib/Perl/Maven.pm
+++ lib/Perl/Maven.pm
@@ -1409,7 +1409,7 @@ sub is_free {
 
 sub is_bot {
 	my $user_agent = request->user_agent || '';
-	return $user_agent =~ /Googlebot|AhrefsBot|TweetmemeBot|bingbot|YandexBot/;
+	return $user_agent =~ /Googlebot|AhrefsBot|TweetmemeBot|bingbot|YandexBot|MJ12bot|heritrix|Baiduspider/;
 }
 
 true;
