076cd307180b0ca381d5f4c735a1c2f312282621
Gabor Szabo <gabor@szabgab.com>
2014-12-02 13:26:10 +0200

    switch back to 90 per year

--- lib/Perl/Maven/PayPal.pm
+++ lib/Perl/Maven/PayPal.pm
@@ -147,9 +147,9 @@ sub paypal_buy {
 			$params{t1} = 'M';
 		}
 		if ( $type eq 'annual' ) {    # TODO remove hardcoding
-			$params{a1} = 60;
-			$params{p1} = 1;
-			$params{t1} = 'Y';
+			#$params{a1} = 60;
+			#$params{p1} = 1;
+			#$params{t1} = 'Y';
 			$usd        = 90;
 			$params{a3} = $usd;
 			$params{t3} = 'Y';        # yearly
