f7af11a93014ac6a6569d02469fab405e2ab1ab5
Gabor Szabo <gabor@szabgab.com>
2014-12-07 22:59:17 +0200

    use Email::Stuffer

--- lib/Perl/Maven/Sendmail.pm
+++ lib/Perl/Maven/Sendmail.pm
@@ -4,11 +4,8 @@ use warnings;
 
 our $VERSION = '0.11';
 
-use Email::Sender::Simple qw(sendmail);
+use Email::Stuffer;
 use Email::Sender::Transport::SMTP qw();
-use Email::MIME::Creator;
-
-use Try::Tiny;
 
 use Exporter qw(import);
 our @EXPORT_OK = qw(send_mail html2text);
@@ -16,57 +13,23 @@ our @EXPORT_OK = qw(send_mail html2text);
 sub send_mail {
 	my ( $header, $raw_content ) = @_;
 
-	my %content = %$raw_content;
+	my $html    = $raw_content->{html};
+	my $text    = $raw_content->{text} || html2text($html);
+	my $subject = delete $header->{Subject};
+	my $from    = delete $header->{From};
+	my $to      = delete $header->{To};
 
-	$content{text} ||= html2text( $content{html} );
-
-	my %type = (
-		text => 'text/plain',
-		html => 'text/html',
-	);
-
-	my @parts;
-	foreach my $t (qw(html text)) {
-		push @parts,
-			Email::MIME->create(
-			attributes => {
-				content_type => $type{$t},
-				( $t eq 'text' ? ( disposition => 'attachment' ) : () ),
-				encoding => 'quoted-printable',
-				charset  => 'UTF-8',
-			},
-			body_str => $content{$t},
-			);
-		$parts[-1]->charset_set('UTF-8');
-	}
-
-	my $msg = Email::MIME->create(
-		header_str => [
-			%$header,
-			'Type'    => 'multipart/alternative',
-			'Charset' => 'UTF-8',
-		],
-		parts => \@parts,
-	);
-	$msg->charset_set('UTF-8');
-
-	try {
-		sendmail(
-			$msg,
+	my $email = Email::Stuffer->text_body($text)->html_body($html)->subject($subject)->from($from)->transport(
+		Email::Sender::Transport::SMTP->new(
 			{
-				transport => Email::Sender::Transport::SMTP->new(
-					{
-						host => 'localhost',
-					}
-				)
+				host => 'localhost',
 			}
-		);
+		)
+	);
+	foreach my $key ( keys %$header ) {
+		$email->header( $key, $header->{$key} );
 	}
-	catch {
-		warn "sending failed: $_";
-	};
-
-	return;
+	$email->to($to)->send_or_die;
 }
 
 sub html2text {
