2083f26f9945596549c75e0fe9e5be1d429c6155
Gabor Szabo <gabor@szabgab.com>
2014-12-17 19:19:50 +0200

    work-around to invoke the search when user click with mouse

--- public/javascripts/perlmaven.js
+++ public/javascripts/perlmaven.js
@@ -164,12 +164,20 @@ $(document).ready(function() {
 	    mysearch(e.target.value, false);
 	});
 
-	$('#typeahead').typeahead( { 'source' : setup_search, items : 20  });
+	$('#search_box').typeahead( { 'source' : setup_search, items : 20  });
+	$('#search_box').blur(function (e) {
+		// This delay is a work-around to let the JS fill the input box before we grab the value from there.
+		// In later versions of Bootstrap there is probably a better solution for this
+		setTimeout(function() {
+			var keyword = $("#search_box").val();
+			//console.log('click:' + keyword);
+			mysearch(keyword, true);
+		}, 100);
+	});
 
-	$("#typeahead").keyup(function (e) {
+	$("#search_box").keyup(function (e) {
 	    if (e.keyCode == 13) {
-	        var keyword = $("#typeahead").val();
-
+	        var keyword = $("#search_box").val();
 	        mysearch(keyword, true);
 		}
 	});
