fff689a3057bebeaa87ac2eef765c8b310c79803
Gabor Szabo <gabor@szabgab.com>
2015-07-02 08:11:01 +0300

    embed youtube

--- lib/Perl/Maven/Page.pm
+++ lib/Perl/Maven/Page.pm
@@ -134,6 +134,11 @@ sub process {
 
 	while ( @{ $self->raw } ) {
 		my $line = shift @{ $self->raw };
+		if ( $line =~ m{^\s*<(youtube)\s+id="(.*)"\s+/>\s*$} ) {
+			my $youtube = $2;
+			$line = qq{<iframe width="1023" height="576" src="http://www.youtube.com/embed/$youtube" frameborder="0" allowfullscreen></iframe>}
+		}
+
 		if ( $line =~ m{^\s*<(screencast|slidecast)\s+file="(.*)"\s+/>\s*$} ) {
 			my ( $type, $file ) = ( $1, $2 );
 			my $path = substr $file, length('/media');
