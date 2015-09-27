a8ffa7fb9ce29ea177e766e3fe6e9a7ceb36db83
Gabor Szabo <gabor@szabgab.com>
2015-09-27 13:43:15 +0300

    older mongodb

--- lib/Perl/Maven/Analyze.pm
+++ lib/Perl/Maven/Analyze.pm
@@ -97,7 +97,13 @@ sub fetch_cpan {
 		}
 
 		$db->delete_many( { project => $r->distribution, version => $r->version } );
-		$db->insert_many( \@docs );
+		foreach my $d (@docs) {
+			$db->insert($d);
+		}
+
+		# not yet supported on the server
+		#$db->insert_many( \@docs );
+		#$db->insert_many( \@docs );
 		$self->mongodb('perl_projects')->insert_one( { project => $r->distribution } );
 	}
 	return;
