9d2343bae44ac032110813407b7e15c81434605c
Gabor Szabo <gabor@szabgab.com>
2016-10-19 17:11:10 +0300

    mp3 counter

---
+++ bin/mp3count.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+use Data::Dumper;
+
+die "Usage: $0 /var/log//nginx/code-maven.com.log \n" if not @ARGV;
+
+foreach my $file (@ARGV) {
+	die "Cannot handle $file\n" if $file !~ /\.log(\.\d)?$/;
+	open my $fh, '<', $file or die;
+	my %download;
+	my %seen;
+	while (my $line = <$fh>) {
+		#next if $line !~ /\.mp3/;
+		my ($ip, $mp) = $line =~ m{^(\S+)\s.*?GET\s+\S+/([^/]+\.mp[34])\s};
+		next if not $mp; # e.g. HEAD instead of GET
+		#die $line if not $mp3;
+		next if $seen{$mp}{$ip}++;
+		$download{$mp}++;
+	}
+	print Dumper \%download;
+}
+
