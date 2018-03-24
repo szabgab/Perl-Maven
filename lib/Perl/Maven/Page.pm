eb293a74f0079132cfb9a5047abc0637788178e0
Gabor Szabo <gabor@szabgab.com>
2018-03-24 09:24:14 +0300

    allow =sample in the header for the ebook conversion

--- lib/Perl/Maven/Page.pm
+++ lib/Perl/Maven/Page.pm
@@ -22,7 +22,7 @@ has pre    => ( is => 'ro', default  => sub { {} } );
 has inline => ( is => 'ro', default  => sub { [] } );
 
 my @page_options
-	= qw(title timestamp author status description? indexes@? tags@? mp3@? original? books@? translator? redirect? perl6url? perl6title? img? alt?);
+	= qw(title timestamp author status description? indexes@? tags@? mp3@? original? books@? translator? redirect? perl6url? perl6title? img? alt? sample?);
 my @common_options
 	= qw(archive? comments_disqus_enable? show_social? show_newsletter_form? show_right? show_related? show_date? show_ads?);
 my @header = ( @page_options, @common_options );
