8d1f0417c046cbf437e908b9cc4ec863094f3ab7
Gabor Szabo <gabor@szabgab.com>
2020-10-07 09:30:40 +0300

    add title to courses

--- lib/Perl/Maven.pm
+++ lib/Perl/Maven.pm
@@ -913,7 +913,7 @@ get '/courses/:file' => sub {
 	my $json        = Cpanel::JSON::XS->new->utf8;
 	my $json_string = Path::Tiny::path($json_file)->slurp_utf8;
 	my $data        = $json->decode($json_string);
-	return pm_template 'course', { course => $data };
+	return pm_template 'course', { course => $data, title => $data->{title} };
 };
 
 get '/favicon.ico' => sub {
