5df307cfd55ab158ac6ab31968171af0d5d68239
Gabor Szabo <gabor@szabgab.com>
2021-06-06 10:23:23 +0300

    slashes

--- lib/Perl/Maven.pm
+++ lib/Perl/Maven.pm
@@ -306,7 +306,9 @@ hook before_template => sub {
 		$t->{github} = mymaven->{github};
 		$t->{github_title} = "";
 	    my $host = request->host;
-		$t->{github_body} = "https://$host$path";
+		$t->{github_body} = "$host/$path";
+		$t->{github_body} =~ s{/+}{/}; # remove double slashes
+		$t->{github_body} = "https://$t->{github_body}";
 	}
 
 	if ( $t->{no_such_article} ) {
