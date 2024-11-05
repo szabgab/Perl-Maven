95714c9f2c02dfffb0e3c56a74b6c5abea6db7d5
Gabor Szabo <gabor@szabgab.com>
2024-11-05 09:10:43 +0200

    handle all the fields dufing conversion

--- convert_to_md.pl
+++ convert_to_md.pl
@@ -52,6 +52,23 @@ for my $file (@files) {
             }
             next;
         }
+        if ($line =~ /^=books\s+(.*)/) {
+            my @books = split /\s*,\s*/, $1;
+            push @output, "books:\n";
+            for my $book (@books) {
+                push @output, "  - $book\n";
+            }
+            next;
+        }
+
+        if ($line =~ /^=tags\s+(.*)/) {
+            my @tags = split /\s*,\s*/, $1;
+            push @output, "types:\n";
+            for my $tag (@tags) {
+                push @output, "  - $tag\n";
+            }
+            next;
+        }
 
         if ($line =~ /^=status\s+(.*)/) {
             my $status = $1;
@@ -72,6 +89,29 @@ for my $file (@files) {
             } else {
                 push @output, "author: $author\n";
             }
+            next;
+        }
+
+        if ($line =~ /^=translator\s+(.*)/) {
+            my $translator = $1;
+            if ($translator eq "0") {
+
+            } else {
+                push @output, "translator: $translator\n";
+            }
+            next;
+        }
+
+        if ($line =~ /^=original\s+(.*)/) {
+            my $original = $1;
+            push @output, "original: $original\n";
+            next;
+        }
+
+        if ($line =~ /^=description\s+(.*)/) {
+            my $description = $1;
+            push @output, "description: $description\n";
+            next;
         }
 
 
@@ -79,10 +119,13 @@ for my $file (@files) {
         if ($line =~ /^=archive\s+(.*)/) {
             my $archive = $1;
             if ($archive eq "0") {
+                push @output, "archive: false\n";
             } elsif ($archive eq "1") {
+                push @output, "archive: true\n";
             } else {
                 die "Invalid archive '$archive' in $in_file";
             }
+            next;
         }
 
         if ($line =~ /^=show_related\s+(.*)/) {
@@ -90,13 +133,55 @@ for my $file (@files) {
             if ($show_related eq "0") {
                 push @output, "show_related: false\n";
             } elsif ($show_related eq "1") {
-
+                push @output, "show_related: true\n";
             } else {
                 die "Invalid show_related '$show_related' in $in_file";
             }
+            next;
+        }
+
+        if ($line =~ /^=img\s+(.*)/) {
+            push @output, "img: $1\n";
+            next;
+        }
+        if ($line =~ /^=alt\s+(.*)/) {
+            push @output, "alt: $1\n";
+            next;
+        }
+
+        # =mp3 /media/cmos-19-job-van-achterberg.mp3, 28149915, 28:21
+        if ($line =~ /^=mp3\s+(.*)/) {
+            my @parts = split /, */, $1;
+            die "Invalid line '$line'" if scalar @parts != 3;
+            my ($file, $size, $time) = @parts;
+            push @output, "mp3:\n";
+            push @output, "  file: $file\n";
+            push @output, "  size: $size\n";
+            push @output, "  time: $time\n";
+            next;
         }
 
         #LINE: =comments_disqus_enable 0
+        next if $line =~ /^=show_newsletter_form /;
+        next if $line =~ /^=show_date /;
+        next if $line =~ /^=show_right /;
+        next if $line =~ /^=perl6url /;
+        next if $line =~ /^=perl6title /;
+        next if $line =~ /^=show_social /;
+        next if $line =~ /^=show_ads /;
+        next if $line =~ /^=sample /;
+        next if $line =~ /^=redirect /;
+        next if $line =~ /^=embedded_ad /;
+        next if $line =~ /^=feed /;
+        next if $line =~ /^=comments /;
+        next if $line =~ /^=newsletter /;
+        next if $line =~ /^=published /;
+        next if $line =~ /^=social /;
+        next if $line =~ /^=comments_disqus_enable /;
+
+        if ($line =~ /^=(\S*)/) {
+            die "Unprocessed header field '$1' in '$file'";
+        }
 
         if ($line =~ /^\s*$/) {
             $in_header = 0;
